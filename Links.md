ec9ce42c6653221a3ad6649adb4680816d156b8c
Kristoffer Hempel <kristoffer.hemp@gmail.com>
2020-02-07 12:38:25 +0100

    added tcp server and client

---
+++ Links.md
@@ -0,0 +1,4 @@
+#Helpfull Links
+
+https://severalnines.com/database-blog/real-time-data-streaming-mongodb-change-streams
+https://docs.mongodb.com/manual/changeStreams/
---
+++ server/server.js
@@ -0,0 +1,12 @@
+const net = require('net');
+
+net
+  .createServer(socket => {
+    socket.on('readable', () => {
+      const data = socket.read();
+      if(Buffer.isBuffer(data)) {
+        console.log(data.toString());
+      }
+    });
+  })
+  .listen(8181, 'your-server-ip-here');
