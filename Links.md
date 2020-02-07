63131ecba60ecacc06b27236c69d26157c534c96
Kristoffer Hempel <kristoffer.hemp@gmail.com>
2020-02-07 14:10:19 +0100

    added features

--- Links.md
+++ Links.md
@@ -2,3 +2,5 @@
 
 https://severalnines.com/database-blog/real-time-data-streaming-mongodb-change-streams
 https://docs.mongodb.com/manual/changeStreams/
+https://arduinojson.org
+https://www.amazon.de/Anker-PowerCore-leichtere-Powerbank-Smartphones-Schwarz/dp/B019GJLER8/ref=redir_mobile_desktop?_encoding=UTF8&aaxitk=qMMgV11Ixl8UZomJoZmJvg&hsa_cr_id=6364347950302&ref_=sb_s_sparkle
--- esp8266/datatransfer.ino
+++ esp8266/datatransfer.ino
@@ -1,86 +1,65 @@
 #include <ESP8266WiFi.h>
+#include <ArduinoJson.h>
+
+#include "DHT.h"
+#define DHTPIN 2
+#define DHTTYPE DHT22
+DHT dht(DHTPIN, DHTTYPE);
 
 #ifndef STASSID
 #define STASSID "your-ssid"
-#define STAPSK  "your-password"
+#define STAPSK  "yout-wifi-password"
 #endif
 
 const char* ssid     = STASSID;
 const char* password = STAPSK;
 
-const char* host = "djxmmx.net";
-const uint16_t port = 17;
+const char* host = "your-server-ip";
+const uint16_t port = 8181; //Port
 
 void setup() {
-  Serial.begin(115200);
 
-  // We start by connecting to a WiFi network
+  Serial.begin(9600);
 
-  Serial.println();
-  Serial.println();
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
-
-  /* Explicitly set the ESP8266 to be a WiFi-client, otherwise, it by default,
-     would try to act as both a client and an access-point and could cause
-     network-issues with your other WiFi-devices on your WiFi-network. */
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
 
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
-    Serial.print(".");
   }
 
-  Serial.println("");
-  Serial.println("WiFi connected");
-  Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());
+  dht.begin(); //start sensor
 }
 
 void loop() {
-  Serial.print("connecting to ");
-  Serial.print(host);
-  Serial.print(':');
-  Serial.println(port);
-
   // Use WiFiClient class to create TCP connections
   WiFiClient client;
   if (!client.connect(host, port)) {
-    Serial.println("connection failed");
     delay(5000);
     return;
   }
 
-  // This will send a string to the server
-  Serial.println("sending data to server");
+  //read humidity and temperature data
+  float hum = dht.readHumidity();
+  float temp = dht.readTemperature();
+  if (isnan(hum) || isnan(temp)) {
+    Serial.println(F("Failed to read from DHT sensor!"));
+    return;
+  }
+
+  //create a json object with the data form the sensor
+  const int capacity = JSON_OBJECT_SIZE(2);
+  StaticJsonDocument<capacity> doc;
+  doc["temperature"]=temp;
+  doc["humidity"]=hum;
+
+  // This will send the json object to the server
   if (client.connected()) {
-    client.println("hello from ESP8266");
-  }
-
-  // wait for data to be available
-  unsigned long timeout = millis();
-  while (client.available() == 0) {
-    if (millis() - timeout > 5000) {
-      Serial.println(">>> Client Timeout !");
-      client.stop();
-      delay(60000);
-      return;
-    }
-  }
-
-  // Read all the lines of the reply from server and print them to Serial
-  Serial.println("receiving from remote server");
-  // not testing 'client.connected()' since we do not need to send data here
-  while (client.available()) {
-    char ch = static_cast<char>(client.read());
-    Serial.print(ch);
+    serializeJson(doc, client);
   }
 
   // Close the connection
-  Serial.println();
-  Serial.println("closing connection");
   client.stop();
 
-  delay(300000); // execute once every 5 minutes, don't flood remote service
+  delay(5000); // execute once every 5 seconds
 }
